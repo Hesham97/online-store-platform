c9ffe367178d85d2a885c9273a0cb6b68e335871
Hesham97 <hesham_magdy97@hotmail.com>
2017-12-16 14:26:35 +0200

    add product

--- src/onlineStore/ProductDBHandler.java
+++ src/onlineStore/ProductDBHandler.java
@@ -17,7 +17,7 @@ public class ProductDBHandler {
 		}
 	}
 	
-	public void addProduct(String productID,String productName,float productPrice
+	public void addOnlineProduct(String productID,String productName,float productPrice
 			,int quantity,float discount,String category,String storeID) throws SQLException {
 		
 	/*	
@@ -40,4 +40,34 @@ public class ProductDBHandler {
 		stmt.executeUpdate(insertQuery);
 
 	}
+	
+	
+
+	public void addOflineProduct(String productID,String productName,float productPrice
+			,int quantity,float discount,String category,String storeID) throws SQLException {
+		
+	/*	
+	 | PRODUCTID       | varchar(100) | NO   | PRI | NULL    |       |
+| ONLINESTORENAME | varchar(50)  | NO   | PRI | NULL    |       |
+| PRODUCTPRICE    | float        | YES  |     | NULL    |       |
+| PRODUCTQUANTITY | int(11)      | YES  |     | NULL    |       |
+| PRODUCTDISCOUNT | float        | YES  |     | NULL    |       |
+| numberOfViews   | int(11)      | YES  |     | NULL    |       |
+| CATEGORYNAME    | varchar(20)  | YES  | MUL | NULL   
+
+*/
+		String insertQuery = "INSERT INTO OFFLINESTOREPRODUCT (PRODUCTID,ONLINESTORENAME,PRODUCTPRICE,"
+				+ "PRODUCTQUANTITY,PRODUCTDISCOUNT,numberOfViews,CATEGORYNAME)"
+				+ "VALUES " + "('" + (productID+storeID) + "', '" + storeID + "',"
+				+"'"+productPrice+"','"+quantity+"','"+discount+"',0,'"+category+"');";
+		System.out.println("Creating statement...");
+		Statement stmt = DB.createStatement();
+
+		stmt.executeUpdate(insertQuery);
+
+	}
+	public void updateView(String productID) {
+		
+	}
+
 }

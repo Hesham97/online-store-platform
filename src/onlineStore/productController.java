875f66e853d69045281bb7b93263fc6088dd5063
Radwan <AhmedRadwanGames@gmail.com>
2017-12-17 17:23:00 +0200

    merged

--- src/onlineStore/productController.java
+++ src/onlineStore/productController.java
@@ -1,7 +1,86 @@
 package onlineStore;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class productController {
 
-//  public Product[] Search(Double price, String brandID, String category, String name, Boolean bestsales);
+	ProductDBHandler productDBHandler = new ProductDBHandler();
+	
+	public ArrayList<Product> Search(float price, String brandID, String category, String name) throws SQLException {
+		ArrayList<Product> product = null;
+		ArrayList<Product> productprice = null;
+		ArrayList<Product> productbrand = null;
+		ArrayList<Product> productcategory = null;
+		ArrayList<Product> productname = null;
+		
+		boolean start = false;
+		
+		if(brandID != "") {
+			productbrand = productDBHandler.getProductByBrand(brandID);
+			SortByID(productbrand);
+			if(!start) {
+				product = productbrand;
+			}
+			start = true;
+		}
+		if(price != -1) {
+			productprice = productDBHandler.getProductByPrice(price);
+			SortByID(productprice);
+			if(!start) {
+				product = productprice;
+			}else {
+				
+			}
+			start = true;
+		}
+		if(category != "") {
+			productcategory = productDBHandler.getProductByCategory(category);
+			SortByID(productcategory);
+			if(!start) {
+				product = productcategory;
+			}else {
+				
+			}
+			start = true;
+		}
+		if(name != "") {
+			productname = productDBHandler.getProductByName(name);
+			SortByID(productname);
+			if(!start) {
+				product = productname;
+			}else {
+				
+			}
+			start = true;
+		}
+	
+		return product;
+	}
+	
+	private void AddExist(ArrayList<Product> from ,ArrayList<Product> to) {
+		for(int i = 0 ; i < from.size() ; i++) {
+			for(int j = 0 ; j < to.size() ; j++) {
+				if(from.get(i).productID == to.get(j).productID)
+				{
+					to.remove(j);
+					break;
+				}
+			}
+		}
+	}
+	
+	private void SortByID(ArrayList<Product> list) {
+		for(int i = 0 ; i < list.size() ;i++) {
+			for(int j = i ; j< list.size() ; j++) {
+				if(list.get(i).productID.compareTo(list.get(j).productID) > 0 || list.get(j).tybe){
+					Product temp = list.get(i);
+					list.set(i,list.get(j));
+					list.set(j,temp);
+				}
+			}
+		}
+	}
 //
 //  public Product ViewProduct(String prouctID);
 //
